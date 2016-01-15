0c8ed8b85382059bd917a3efdc82fe5a26b4db73
Turysbek Nazym <nazym_turysbekova@mail.ru>
2016-01-15 09:35:21 +0600

    Kod is changed

--- ConsoleApplication16/Program.cs
+++ ConsoleApplication16/Program.cs
@@ -6,17 +6,38 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication16
 {
-    class Program
+    class Student
     {
-        static void Main(string[] args)
+        public string name, surename;
+        public int age;
+        public string ID;
+        public double gpa;
+        public void printFullName()
         {
-            string s;
-            s = Console.ReadLine();
-            Console.WriteLine(s);
-            Console.ReadKey();
-
-
-            
+            Console.WriteLine(this.name + "" + this.surename);
         }
     }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Student a = new Student();
+            a.name = "Nazym";
+            a.surename = "Turysbek";
+            a.age = 18;
+            a.gpa = 4.0;
+            a.ID = "15BD02078";
+            Student b = new Student();
+            b.name = "Nauryz";
+            b.surename = "Turysbekova";
+            b.age = 20;
+            b.gpa = 3.5;
+            a.printFullName();
+            b.printFullName();
+            Console.ReadKey();
+        }
+    }
+
 }

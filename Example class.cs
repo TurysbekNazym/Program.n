bfd1897f52604f855d6ff3c2066711be3046a491
Turysbek Nazym <nazym_turysbekova@mail.ru>
2016-01-16 23:16:12 +0600

    Class example

---
+++ Example class.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example 
+{
+    class SummerJob
+    {
+        public string name;
+        public int age;
+        public string status;
+        public string a, b, c;
+        public string day;
+        
+        public bool Flag;
+        public void JobFinder()
+        {
+            Console.WriteLine(this.name + "" + this.age+""+this.status+""+this.a,b,c);
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+             SummerJob P= new SummerJob();
+             Console.WriteLine("Hello,Please enter you name:");
+             P.name = Console.ReadLine();
+             Console.WriteLine("Please enter your age");
+             P.age = int.Parse (Console.ReadLine());
+            
+            if (P.age > 16 && P.age < 65)
+            {
+                Console.WriteLine("WE find one job for you");
+                Console.WriteLine("Who you are? :student ,worker  ,pensioner ");
+                P.status = Console.ReadLine();
+
+               P.Flag = (P.status==P.a);
+               if (P.Flag)
+               {
+                   Console.WriteLine(" waiter in cafe");
+               }
+
+             //  else
+               //    Console.WriteLine("Baby sitter");
+            }
+            if (P.age > 12 && P.age < 16)
+            {
+                Console.WriteLine("Sorry , you are child");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}

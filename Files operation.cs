21d053250e7a20f6a7bc83137ef0a544463fa528
Turysbek Nazym <nazym_turysbekova@mail.ru>
2016-01-21 23:46:15 +0600

    Added StreamWriter

--- Files operation.cs
+++ Files operation.cs
@@ -19,8 +19,11 @@ namespace work with files
             //reader.Close();
             //Console.ReadLine();
           //  File.Create(@"C:\Users\Администратор\Downloads\Nazym.docx");
-            File.Delete(@"C:\Users\Администратор\Downloads\Nazym.docx");
-            
+            //File.Delete(@"C:\Users\Администратор\Downloads\Nazym.docx");
+            FileStream file= new FileStream (@"C:\Users\Администратор\Downloads\Nazym.docx",FileMode.Create);
+            StreamWriter writer= new StreamWriter (file);
+            writer.Write("tekst");
+            writer.Close();
 
 
         }

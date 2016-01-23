6a8d9845f7278360f608d969dd1e229b7aed98e8
Turysbek Nazym <nazym_turysbekova@mail.ru>
2016-01-23 14:43:33 +0600

    Lab1 class Student

---
+++ Lab1 class Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Student
+{
+    class Student//создаем класс студент
+    {   public string name,surname;//  методы 
+        public double  gpa;
+
+       public Student (string name,string surname,double gpa)// создаем конструктор
+       {
+           this.name = name;
+           this.surname = surname;
+           this.gpa = gpa;
+        }
+
+         public override string ToString()// все функиций можно переписать  
+         {
+              return this.name+""+this.surname+""+this.gpa;
+         }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student a = new Student("Nazym", "Turysbek", 4.0);// создаем экземпляры от класс студент
+            Console.WriteLine(a);
+            Console.ReadKey();
+
+ 
+        }
+    }
+}

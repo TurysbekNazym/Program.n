1beb964c170fa25a19058b4a9d3271ed3b2cd35c
Turysbek Nazym <nazym_turysbekova@mail.ru>
2016-01-29 12:16:36 +0600

    Min prime

---
+++ Lab2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab2_lower_Prime
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            FileStream FF = new FileStream(@"C:\Users\Администратор\Desktop\Nazym\lab.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream FF2=new FileStream (@"C:\Users\Администратор\Desktop\Nazym\lab2.txt",FileMode.OpenOrCreate,FileAccess.ReadWrite);
+            StreamReader aa= new StreamReader(FF);
+            StreamWriter qq=new StreamWriter (FF2);
+            int cnt = 0;
+            int min = 0;
+            bool flag = false;
+            string [] s=Console.ReadLine().Split();
+
+            foreach (string h in s)
+            {
+                int number = int.Parse(h);
+                cnt = 0;
+                for (int i = 2; i <= number; i++)
+                {
+
+
+                    if (number % i == 0)
+                    {
+                        cnt++;
+                    }
+                    if (cnt >= 2)
+                    {
+                        break;
+
+                    }
+                }
+                if (cnt == 1)
+                {
+                    if (!flag)
+                    {
+                        min = number;
+                    }
+                    if (min > number)
+
+                        min = number;
+                }
+            }
+            Console.WriteLine(min);
+            qq.WriteLine(min);
+            aa.Close();
+            qq.Close();
+            FF.Close();
+            FF2.Close();
+            Console.ReadKey();
+        }   
+               
+
+                
+            
+
+        
+     } 
+}

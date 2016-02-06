effa9642f2d1cd1f57b223e520226c73d7cf6f01
Turysbek Nazym <nazym_turysbekova@mail.ru>
2016-02-06 17:21:44 +0600

    Second task was done

--- GAME GAME/GAME GAME/Model/Food.cs
+++ GAME GAME/GAME GAME/Model/Food.cs
@@ -17,7 +17,48 @@ namespace GAME_GAME.Model
         {
             int x = new Random().Next() % 20;
             int y = new Random().Next() % 20;
+            bool foodgen = false;
+            while (!foodgen)
+            {
+                 x = new Random().Next(0,20);
+                 y = new Random().Next(0,20);
+                 foodgen = true;
+                 for (int i = 0; i < Game.wall.body.Count; i++)// cheking with wall
+                 {
+                     if (x == Game.wall.body[i].x && y == Game.wall.body[i].y)// if coordinates of food is equal to the  wall coord do these
+                     {
+                         foodgen = false;
+                         break;
+                     }
+
+                 }
+
+                 for (int i = 0; i < Game.snake.body.Count; i++)// cheking with snake position 
+                 {
+                     if (x == Game.snake.body[i].x && y == Game.snake.body[i].y)//  food VS snake 
+
+                     {
+                         foodgen = false;// we could not put here food 
+                         break;// loop is over 
+                     }
+                 }
+            }
+            if (body.Count == 0)
+                body.Add(new Point(x, y));
+            else
+            {
+                body[0].x = x;
+                body[0].y = y;
+            }
             
+
+
+
+
+
+
+
+
             if (body.Count == 0)
                 body.Add(new Point(x, y));
             else

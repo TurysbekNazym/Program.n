9c63c64260ab69d52df97429207ac54722c7a9a0
Turysbek Nazym <nazym_turysbekova@mail.ru>
2016-03-13 18:34:31 +0600

    Thread S

---
+++ THREAD Subject/THREAD Subject/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace THREAD_Subject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread tr1 = new Thread(Sum);
+            tr1.Name = "Move_do";
+       
+            Thread tr2=new Thread(LO);
+            tr2.Name="fly_lay";
+            tr1.Start();
+            tr1.Join();
+            tr2.Start();
+            Console.ReadKey();
+        }
+        public  static void Sum()
+        {
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name +" "+i);
+            }
+        }
+        public static  void LO()
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(Thread.CurrentThread.Name + " " + i);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(Thread.CurrentThread.Name + " " + i);
+                }
+
+            }
+        }
+    }
+}

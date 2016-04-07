00ca615f1fc827746cf5300affe90f8bee28b64b
Turysbek Nazym <nazym_turysbekova@mail.ru>
2016-04-07 23:29:19 +0600

    paint

---
+++ paint_with_picturebox/paint_with_picturebox/Drawer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+namespace paint_with_picturebox
+{
+  //  tolwina i color owirgish kosu
+    public enum Tool { Pencil,Rectangle,Cirle,lactic}
+    class Drawer
+    {
+         public Pen pen;
+         public Pen pen1;
+         public Graphics g;
+         public Bitmap btm;
+         public PictureBox picture;
+         public  Tool tool;
+         public Point prev;
+         public bool started_Paint=false;
+         public Drawer(PictureBox p)
+         {
+            
+             this.picture = p;
+             btm = new Bitmap(picture.Width, picture.Height);
+             g = Graphics.FromImage(btm);
+             picture.Image = btm;
+             pen = new Pen(Color.Black,2);
+            pen1= new Pen(Color.White,15);
+
+
+         }
+         public void Draw(Point cur)
+         {
+             switch (tool)
+             {
+
+                 case Tool.Pencil:
+                 g.DrawLine(pen, prev, cur);
+                 prev = cur;
+                 break;
+                 case Tool.Rectangle:
+                 break;
+                 case Tool.Cirle:
+                 break;
+                 case Tool.lactic:
+                 g.DrawLine(pen1, prev, cur);
+                 prev = cur;
+                 break;
+             }
+             picture.Refresh();
+         }
+   }
+
+
+
+}
---
+++ paint_with_picturebox/paint_with_picturebox/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paint_with_picturebox
+{
+    public partial class Form1 : Form
+    {
+        Drawer drawer;
+        public Form1()
+        {
+            InitializeComponent();
+            drawer = new Drawer(pictureBox1);
+
+            
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawer.prev = e.Location;
+            drawer.started_Paint = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawer.started_Paint)
+            {
+                drawer.Draw(e.Location);
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawer.started_Paint = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drawer.tool = Tool.Pencil;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            drawer.tool = Tool.lactic;
+        }
+    }
+}

5f8d73f9d7abebc2e83c5be4c5cb9c2695defb9d
Turysbek Nazym <nazym_turysbekova@mail.ru>
2016-04-23 08:57:06 +0600

    move

---
+++ second_task_move/second_task_move/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace second_task_move
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 310);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.re_paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ second_task_move/second_task_move/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace second_task_move
+{
+    public partial class Form1 : Form
+    {
+        SolidBrush br;
+        Timer tr;
+        float x = 50;
+        float y = 50;
+        float w = 25;
+        float h = 25;
+        float dx = 10;
+        float dy = 10;
+        public Form1()
+        {
+            br = new SolidBrush(Color.Blue);
+            tr = new Timer();
+            tr.Tick += new EventHandler(timer1_Tick);
+            tr.Start();
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (x + w > Width || x+w==Width)
+            {
+                dx = -10;
+                br = new SolidBrush(Color.Black);
+                
+            }
+            else if (x < 0||x==0)
+            {
+                dx = 10;
+                br = new SolidBrush(Color.Brown);
+
+            }
+            else if (y + h > Height || y + h == Height)
+            {
+                dy = -10;
+                br = new SolidBrush(Color.Red);
+            }
+            else if (y < 0 || y == 0)
+            {
+                dy = 10;
+                br = new SolidBrush(Color.Silver);
+            }
+            x += dx;
+            y += dy;
+            Refresh();
+        }
+
+        private void re_paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.FillEllipse(br, x, y, w, h);
+            
+        }
+    }
+}
